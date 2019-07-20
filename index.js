ab3bbe10cd59eea7e29180f2a57868c4e880f050
Jason <jbddelatorre@gmail.com>
2019-07-20 16:53:51 +0800

    Added cors, updated allowed origin

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000;
+const cors = require('cors')
+
+const port = process.env.PORT || 3001;
 
 // const mongoose = require('mongoose')
 const MongoClient = require('mongodb').MongoClient
@@ -17,20 +19,47 @@ MongoClient.connect(DB_URL, { useNewUrlParser: true }, (err, client) => {
 	db = client.db(DB_NAME)
 })
 
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 
-app.get('/', async (req, res) => {
+
+app.get('/hero_counters', async (req, res) => {
 	
 	try {
-		const hc = await db.collection('hero_counters')
-		hc.find({}).toArray((err, items) => {
-			console.log(items) 
+		db.listCollections().toArray(function(err, collInfos) {
+		    // collInfos is an array of collection info objects that look like:
+		    // { name: 'test', options: {} }
+		    console.log(collInfos)
+		});
+
+		const counters = await db.collection('hero_counters')
+		counters.find({}).toArray((err, items) => {
 			res.json(items)
 		})
 		
 	} catch(err) {
 		console.log('err: ' + err)
+
+		return res.json({ error: 'DB Query failed' })
 	}
 })
+
+app.get('/hero_combos', async (req, res) => {
+	
+	try {
+		const combos = await db.collection('hero_combos')
+		combos.find({}).toArray((err, items) => {
+			res.json(items)
+		})
+		
+	} catch(err) {
+		console.log('err: ' + err)
+
+		return res.json({ error: 'DB Query failed' })
+	}
+})
+
 
 app.listen(port, () => {
 	console.log('Listening to port' + port)
